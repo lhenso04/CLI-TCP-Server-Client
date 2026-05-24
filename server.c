e84db198d3367c1a93d415ab8afd4b9cec4d4dd7
Lucas Henson <lucashenson04@gmail.com>
2026-05-23 21:40:19 -0700

    Created the socket, working on binding an address and a port to it next.

--- server.c
+++ server.c
@@ -2,10 +2,31 @@
 // Written by LH May '26
 
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(void) {
+  
+  // Will hold both the server's, and an incoming client's file descriptors
+  int socket_fd, csocket_fd;
 
-  puts("Hello, world!");
+  const int socket_port = 8080;
+
+  struct sockaddr_in socket_info; 
+
+  // First, a socket is created, and the file descriptor is returned
+  socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+
+  if(socket_fd == -1) {
+
+    perror("Failed to create the socket, please try again.");
+
+    return 1;
+
+  }
+
+  // Next, bind an address and port to the newly created socket
+  
 
   return 0;
 
