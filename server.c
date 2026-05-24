de935ed4e4d60efebb4da77b5b5241a3366ebbe0
Lucas Henson <lucashenson04@gmail.com>
2026-05-23 22:50:36 -0700

    Bound an address (local, for now) and a port to the socket.

--- server.c
+++ server.c
@@ -2,6 +2,7 @@
 // Written by LH May '26
 
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -12,7 +13,8 @@ int main(void) {
 
   const int socket_port = 8080;
 
-  struct sockaddr_in socket_info; 
+  struct sockaddr_in socket_info;
+  memset(&socket_info, 0, sizeof(socket_info)); // For compatibility
 
   // First, a socket is created, and the file descriptor is returned
   socket_fd = socket(PF_INET, SOCK_STREAM, 0);
@@ -26,6 +28,19 @@ int main(void) {
   }
 
   // Next, bind an address and port to the newly created socket
+  socket_info.sin_family = AF_INET; // IPv4
+  socket_info.sin_port = htons(socket_port); // Port: 8080
+  socket_info.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // Address: 127.0.0.1 (localhost)
+  
+  if(bind(socket_fd, (struct sockaddr *)&socket_info, sizeof(socket_info)) == -1) {
+
+    perror("Failed to bind the address and port to the socket!");
+
+    return 1;
+
+  }
+
+  // Have the socket start listening on the bound address and port
   
 
   return 0;
