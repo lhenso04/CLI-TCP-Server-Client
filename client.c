93094dc43fce22f1f30e480a7ec7e0a0862a2290
Lucas Henson <lucashenson04@gmail.com>
2026-05-24 13:05:51 -0700

    Started work on a simple client to connect to the server and display the sent message.

---
+++ client.c
@@ -0,0 +1,14 @@
+// A very simple TCP client implementation written in the C programming language.
+// Written to get more familiar with the process of connecting to a server via sockets.
+// Written by LH May '26
+
+#include <stdio.h>
+
+int main(void) {
+
+  puts("Hello, world!");
+
+  return 0;
+
+}
+
