9b9eda44e0f4f323468fcbd2c0481063c2e79640
Lucas Henson <lucashenson04@gmail.com>
2026-07-29 22:40:48 -0700

    Added some logic to handle SIGINT cleanly, and also found a few spots where I was not closing the socket file descriptor before exiting the program.

--- src/server.c
+++ src/server.c
@@ -4,9 +4,21 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
+// Variable used to handle SIGINT gracefully
+volatile sig_atomic_t ctrl_c_interrupt = 0;
+
+void handle_sigint(int sig) {
+
+  (void)sig; // To avoid an unused variable warning
+
+  ctrl_c_interrupt = 1;
+
+}
 
 int main(void) {
   
@@ -24,6 +36,12 @@ int main(void) {
   const char* msg = "Hello from the server!";
   const size_t msg_len = strlen(msg);
 
+  // Signal handler for SIGINT
+  struct sigaction act;
+  memset(&act, 0, sizeof(act));
+  act.sa_handler = handle_sigint;
+  sigaction(SIGINT, &act, NULL);
+
   // First, a socket is created, and the file descriptor is returned
   socket_fd = socket(PF_INET, SOCK_STREAM, 0);
 
@@ -35,6 +53,10 @@ int main(void) {
 
   }
 
+  // Allows for reuse of local addresses, prevents the "Address already in use" error if the server is restarted very quickly 
+  int opt = 1;
+  setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
   // Next, bind an address and port to the newly created socket
   socket_info.sin_family = AF_INET; // IPv4
   socket_info.sin_port = htons(socket_port); // Port: 8080
@@ -43,6 +65,8 @@ int main(void) {
   if(bind(socket_fd, (struct sockaddr *)&socket_info, socket_info_len) == -1) {
 
     perror("Failed to bind the address and port to the socket!");
+    
+    close(socket_fd);
 
     return 1;
 
@@ -53,6 +77,8 @@ int main(void) {
 
     perror("Failed to start listening!");
 
+    close(socket_fd);
+
     return 1;
 
   }
@@ -60,16 +86,18 @@ int main(void) {
   printf("Now listening on port %u... (Press Ctrl + C to quit)\n", socket_port); 
 
   // Loop to handle incoming connections
-  while(1) {
+  while(!ctrl_c_interrupt) {
     
     // Accept an incoming connection
     csocket_fd = accept(socket_fd, (struct sockaddr *)&socket_info, &socket_info_len);
 
     if(csocket_fd == -1) {
+      
+      if (ctrl_c_interrupt) break;
 
       perror("Failed to accept a new connection!");
 
-      return 1;
+      continue;
 
     } 
 
@@ -84,7 +112,9 @@ int main(void) {
 
       perror("Failed to send over the connection!");
 
-      return 1;
+      close(csocket_fd);
+
+      continue;
 
     }
 
@@ -93,9 +123,10 @@ int main(void) {
     // Close the connection
     close(csocket_fd);
 
-    puts("Connection closed!");
-
   }
+
+  // Closes the server socket cleanly after Ctrl + C is pressed
+  if(socket_fd != -1) close(socket_fd);
 
   return 0;
 
